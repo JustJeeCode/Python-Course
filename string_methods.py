9c0100d28eda57acb454961bebfef551d7d9157a
JustJeeCode <michellestanley@michelles-air.home>
2023-04-07 15:21:43 +1000

    added string_methods.py

---
+++ string_methods.py
@@ -0,0 +1,12 @@
+name = "JustJee"
+
+#print(len(name))
+#print(name.find('t'))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha()) # checks to see if str contains only letters
+# print(name.count('J'))
+# print(name.replace("J","R"))
+# print(name * 3) # multiples str 3 times
