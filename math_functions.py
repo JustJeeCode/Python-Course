2dd6f777320daae8393646fb198417049f9bea95
JustJeeCode <michellestanley@michelles-air.home>
2023-04-07 15:40:55 +1000

    added math_functions.py

---
+++ math_functions.py
@@ -0,0 +1,15 @@
+import math
+
+pi = -3.14
+x = 1
+y = 2
+z = 3
+
+#print(round(pi)) # rounds to whole number
+#print(math.ceil(pi)) # rounds a number up to the nearest whole int
+#print(math.floor(pi)) # rounds a number down to the nearest whole int
+#print(abs(pi)) # tells you how far away a number is from 0
+#print(pow(pi, 2)) # raises a number to the power of x
+#print(math.sqrt(420)) # square root of a number
+#print(max(x, y, z)) # finds largest value
+#print(min(x, y, z)) # finds smallest value
