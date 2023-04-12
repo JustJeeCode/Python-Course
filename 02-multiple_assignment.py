1c254220ce53ffd24db1506ac3d84282ecb436ea
JustJeeCode <michellestanley@michelles-air.home>
2023-04-12 19:55:14 +1000

    Added documentation to file

--- 02-multiple_assignment.py
+++ 02-multiple_assignment.py
@@ -1,22 +1,30 @@
-# Multiple assignment = allows us to assign multiple variables at the 
-# same time in one line of code.
+"""
 
-# Standard variable assignment:
-# name = "Jee"
-# age = 25
-# attractive = True
+Multiple assignment = allows us to assign multiple variables at the 
+same time in one line of code.
 
-# Multiple assignment:
-# name, age, attractive = "Jee", 25, True 
 
-# print(name)
-# print(age)
-# print(attractive)
+Standard variable assignment:
 
-# Another type of multiple assignment
+name = "Jee"
+age = 25
+attractive = True
+
+
+Multiple assignment:
+
+name, age, attractive = "Jee", 25, True 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Here we are assigning name = "Jee", age = 25, attractive = True.
+This works based on the order of variable names and values. Say for example we changed
+the values "Jee" with 25 well then name = 25 and age = "Jee", the order is what matters.
+
+
+Another type of multiple assignment:
+
 Spongebob = Patrick = Sandy = Squidward = 30
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Here we are assigning all variables here: Spongebob, Patrick, Sandy and Squidward
+to the int value 30.
 
-print(Spongebob)
-print(Patrick)
-print(Sandy)
-print(Squidward)
+"""
