d58609b0043e7f04694c32d01b6985679684c92f
JustJeeCode <michellestanley@michelles-air.home>
2023-04-13 09:24:50 +1000

    Added documentation to file

--- 05-user_input.py
+++ 05-user_input.py
@@ -1,10 +1,40 @@
-# Input allows us to assign an input to a variable.
-# e.g. name = input()
+"""
 
-name = input("What is your name?: ")
-age = int(input("How old are you?: "))
-height = float(input("How tall are you?: "))
+Input allows us to call for an input in the console.
 
-print("Hello " + name)
-print("You're " + str(age) + " years old.")
-print("You're " + str(height) + "cm tall.")
+It's best practice to assign them to a variable otherwise there 
+would be no stored data from the input.
+
+SYNTAX:
+
+input() <- Also to submit an input you need to press 'ENTER'
+
+ASSIGNING IT TO A VARIABLE:
+
+name = input()
+
+
+Here we are assigning the variable name to the input "What is your name?: ".
+Also by default our input will store as a string value.
+vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+name = input("What is your name?: ") | OUTPUT => "What is your name?: _"
+
+Here we are assigning the variable age to the integer input "How old are you?: ".
+Our input will store as an integer.
+vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+age = int(input("How old are you?: ")) | OUTPUT => "How old are you?: _"
+
+Here we are assigning height to the float input "How tall are you?: ".
+Our input will store as a float.
+vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+height = float(input("How tall are you?: ")) OUTPUT => "How tall are you?: _"
+
+
+print("Hello " + name) | OUTPUT => "Hello [name]"
+
+print("You're " + str(age) + " years old.") | OUTPUT => "You're [AGE] years old."
+print("You're " + str(height) + "cm tall.") | OUTPUT => "You're [HEIGHT] cm tall."
+
+"""
+
+
