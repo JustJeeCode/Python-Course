ab0f0163a6b4d26314102f86a52e4ac38d693d84
JustJeeCode <michellestanley@michelles-air.home>
2023-04-13 09:34:52 +1000

    Added documentation to file

--- 06-math_functions.py
+++ 06-math_functions.py
@@ -1,16 +1,45 @@
-# Importing the math module - gives us more math functions.
+"""
+MATH FUNCTIONS IN PYTHON:
+
+Here we are importing a module. This allows us to gain functions from 
+other files.
+
+We are importing the built-in math module.
+vvvvvvvvvvv
 import math
 
-pi = -3.14
+We are assigning some variables:
+
+pi = 3.14
 x = 1
 y = 2
 z = 3
 
-#print(round(pi)) # Rounds to whole number
-#print(math.ceil(pi)) # Rounds a number up to the nearest whole int
-#print(math.floor(pi)) # Rounds a number down to the nearest whole int
-#print(abs(pi)) # Tells you how far away a number is from 0
-#print(pow(pi, 2)) # Raises a number to the power of x
-#print(math.sqrt(420)) # Square root of a number
-#print(max(x, y, z)) # Finds largest value
-#print(min(x, y, z)) # Finds smallest value
+MATH FUNCTIONS:
+
+Rounds to whole number:
+print(round(pi)) | OUTPUT => 3
+
+Rounds a number up to the nearest whole number:
+print(math.ceil(pi)) | OUTPUT => 4
+
+Rounds a number down to the nearest whole number:
+print(math.floor(pi) | OUTPUT => 3
+
+Tells you how far a number is from 0:
+print(abs(pi)) | OUTPUT => 3.14
+
+Raises a number to the power of x:
+print(pow(pi, 2)) | OUTPUT => 9.85
+
+Finds the square root of a number:
+print(math.sqrt(420)) | OUTPUT => 20.49
+
+Finds the maximum value out of a collection:
+print(max(x, y, z)) | OUTPUT => 3
+
+Finds the minimum value out of a collection:
+print(min(x, y, z)) | OUTPUT => 1
+
+"""
+
