d509ad59518b7d6db686870448de0e8aa62e5619
JustJeeCode <michellestanley@michelles-air.home>
2023-04-12 19:34:25 +1000

    Added documentation to variables.py

--- variables.py
+++ variables.py
@@ -1,25 +1,96 @@
-# Container for a value. Behaves as the value that it contains.
+"""
 
-# We have 4 types of primitive data in Python:
-# string - text "", int - whole number, float - decimal number, boolean - 
-# true or false.
+A variable is a container for a value. Behaves as the value that it contains.
 
-# first_name = "Just" - string
-# last_name = "Jee"
-# full_name = first_name + " " + last_name - Concatenation.
+EXAMPLE: x = y <- x is the variable and y is the value.
 
-# print("Hello " + full_name)
-# # print(type(name)) - We can get the type of our variable.
+We can store a variable as 4 main data types:
 
-# age = 20 - int
-# age += 1 - We can add to our int.
-# print("Your age is: " + str(age)) - Typecast our variable to a string to print.
-# #print(type(age))
+String = which is text (wrapped in quotation marks.)
+EXAMPLE: "hi"
 
-# height = 250.5 - float
-# print("Your height is: " + str(height) + "cm")
-# #print(type(height))
+Integer = a whole number can be positive or negative (no quotation marks.)
+EXAMPLE: 4
 
-human = True # - boolean
-print("Are you a human: " + str(human))
-#print(type(human))
+Float = a decimal number can be positive or negative (no quotation marks.)
+EXAMPLE: 6.424
+
+Boolean = a true or false value (no quotation marks.)
+EXAMPLE: True
+
+
+STRINGS:
+
+first_name = "Just" 
+^^^^^^^^^^^^^^^^^^^
+Here we are assigning the variable first_name to the string "Just".
+
+last_name = "Jee"
+^^^^^^^^^^^^^^^^^
+Here we are assigning the variable last_name to the string "Jee".
+
+full_name = first_name + " " + last_name | Assigning full_name to the string "Just Jee".
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+For our variable full_name we are using concatenation.
+Concatenating means obtaining a new string that contains both of the original strings. 
+In Python, there are a few ways to concatenate or combine strings. 
+The new string that is created is referred to as a string object. 
+In order to merge two strings into a single object, you may use the + operator.
+
+print("Hello " + full_name) | OUTPUT => "Hello Just Jee"
+^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Here we are now printing to the console the string. So we will be able to see
+this text.
+
+print(type(name)) | OUTPUT => str
+^^^^^^^^^^^^^^^^^
+If we ever want to find the data type of a variable we can use
+the type() function.
+
+
+INTEGERS:
+
+age = 20
+^^^^^^^^
+We are assigning age to the data type integer and the value is 20.
+
+age += 1
+^^^^^^^^
+We can add to our integer age with the += operator.
+
+print("Your age is: " + str(age)) | OUTPUT => "Your age is: 21"
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Here we are type casting our integer variable age to a string. Type casting means
+to change the data type of a variable. In print statements you cannot print a string
+and any other data type together, so we are type casting our integer variable age to a 
+string. Since our type cast is in a print statement however this is not a permanent 
+change to age and only effects the variable age in the print statement.
+
+print(type(age)) | OUTPUT => int (type cast wasn't a permanent change.)
+
+
+FLOATS:
+
+height = 250.5 
+^^^^^^^^^^^^^^
+We are assigning height to the data type float and the value is 250.5.
+
+print("Your height is: " + str(height) + "cm") | OUTPUT => "Your height is: 250.5cm"
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+We are concatenating our height variable and type casting its type to a string 
+just for the print statement.
+
+print(type(height)) | OUTPUT => float
+
+
+BOOLEANS:
+
+human = True 
+^^^^^^^^^^^^
+We are assigning human to the boolean value: True
+
+print("Are you a human: " + str(human)) | OUTPUT => "Are you a human: True"
+
+print(type(human)) | OUTPUT => bool
+
+"""
