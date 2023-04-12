9d9a59f40e9bb3517861dec2debb10a4e5fb8f53
JustJeeCode <michellestanley@michelles-air.home>
2023-04-12 11:22:59 +1000

    added lists.py

---
+++ lists.py
@@ -0,0 +1,26 @@
+# List = used to store multiple items in a single variable.
+
+food = ["pizza", "hamburger", "hotdog", "muffin", "pudding"]
+
+# Prints "pudding".
+# print(food[4])
+
+# We change element 0 to "sushi".
+food[0] = "sushi"
+# print(food[0])
+
+# List functions:
+#food.append("ice cream") # We add ice cream to the end of the list.
+#food.remove("hotdog") # We remove hotdog from the list.
+#food.pop() # Removes last element.
+#food.insert(0, "cake") # Replaces an element with a new one.
+#food.sort() # Sorts list alphabetically.
+#food.clear() # Removes all elements of a list.
+
+# Prints all elements in a list on a new line.
+for i in food:
+	print(i)
+
+
+
+
