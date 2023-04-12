f3902651ecb20478b37e08ddd07b63a95ef6f21c
JustJeeCode <michellestanley@michelles-air.home>
2023-04-12 23:02:15 +1000

    Added documentation to file

--- 04-type_cast.py
+++ 04-type_cast.py
@@ -1,12 +1,25 @@
-# Type casting = convert the data type of a value to another data type.
+"""
 
-x = 1 # int
-y = 2.0 # float
-z = "3" # string
+Type casting = Convert the data type of a value to another data type.
 
-# x = str(x) # Changing the int x to a string.
-# y = str(y) # This is perma change.
+Here we have some variables with there data types.
+x = 1 <- int
+y = 2.0 <- float
+z = "3" <- string
 
-print("X is " + str(x)) # Casting the int x to a string so we can print it.
-print("Y is " + str(y))
-print(str(z)*3) # Type casting in a print is a temp change.
+Here we are type casting the variable x which is an integer to a string, this change stays
+until we change it back or to another data type.
+
+x = str(x) | this would now be "x" which is a string.
+
+Here we are type casting our variable y which is a float to a string. Since our type cast
+is in a print statement however this change is only temporary and after the print 
+statement y will still be a float.
+
+print("Y is " + str(y)) | OUTPUT => Y is 2.0
+
+Here we are type casting the string z to an integer then multiplying it by 3.
+
+print(int(z)*3) | OUTPUT => 9
+
+"""
