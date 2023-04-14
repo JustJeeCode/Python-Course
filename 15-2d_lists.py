a7643d05f47a4bf3304f5e1d8e0b31074296daf2
JustJeeCode <michellestanley@michelles-air.home>
2023-04-14 21:59:18 +1000

    Added documentation to file

--- 15-2d_lists.py
+++ 15-2d_lists.py
@@ -1,18 +1,40 @@
-# 2D lists = a list of lists.
+"""
 
-# Here are our 3 lists.
+A 2D list is a list of lists.
+
+
+SYNTAX:
+
+cars = ["ute", "suv", "hatchback"]
+boats = ["cruise ship", "jetski", "canoe"]
+motorcycles = ["harley davidson", "dirt bike"]
+
+vehicals = [cars, boats, motorcycles]
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Here we have the list vehicals which is storing 3 lists.
+
+
+EXAMPLE:
+
+Here are our 3 lists:
+
 drinks = ["coffee", "soda", "tea"]
 dinner = ["pizza", "hamburger", "hotdog"]
 dessert = ["cake", "ice cream"]
 
-# Here is our 2D list food and inside it are all 3 of our lists.
+Here is our 2D list food and inside it are all 3 of our lists.
+
 food = [drinks, dinner, dessert]
 
-# Prints all the elements in every list.
+Here we are printing all the elements in every list:
+
 print(food)
 
-# If we wanna access just 1 list we can use the index.
-print(food[0])
+If we wanna access just 1 list we can use the index.
+print(food[0]) <- 1 index operator.
 
-# If we wanna access an element from inside a list.
-print(food[0][0])
+If we wanna access an element from inside a list.
+print(food[0][0]) <- 2 index operators.
+
+"""
+
