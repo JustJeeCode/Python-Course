e36227afcc4bb0c11b1a00a27003ae87be37ee08
JustJeeCode <michellestanley@michelles-air.home>
2023-04-14 20:59:00 +1000

    Added documentation to file

--- 14-lists.py
+++ 14-lists.py
@@ -1,26 +1,48 @@
-# List = used to store multiple items in a single variable.
+"""
+
+List = used to store multiple items in a single variable.
+
+
+SYNTAX:
 
 food = ["pizza", "hamburger", "hotdog", "muffin", "pudding"]
 
-# Prints "pudding".
-# print(food[4])
 
-# We change element 0 to "sushi".
+We can print or target a specific item of a list with the index
+operators:
+
+print(food[4]) | OUTPUT => "pudding"
+^^^^^^^^^^^^^^
+Remember computers start counting at 0.
+
+
+We can change items in a list.
+We change item 0 to "sushi":
+
 food[0] = "sushi"
-# print(food[0])
 
-# List functions:
-#food.append("ice cream") # We add ice cream to the end of the list.
-#food.remove("hotdog") # We remove hotdog from the list.
-#food.pop() # Removes last element.
-#food.insert(0, "cake") # Replaces an element with a new one.
-#food.sort() # Sorts list alphabetically.
-#food.clear() # Removes all elements of a list.
+print(food[0]) | OUTPUT => "sushi"
 
-# Prints all elements in a list on a new line.
+
+Here are some -
+List functions:
+
+food.append("ice cream") <- We add ice cream to the end of the list.
+food.remove("hotdog") <- We remove hotdog from the list.
+food.pop() <- Removes last element.
+food.insert(0, "cake") <- Replaces an element with a new one.
+food.sort() <- Sorts list alphabetically.
+food.clear() <- Removes all elements of a list.
+
+We can print all items of a list to a new line with a for loop:
+
 for i in food:
 	print(i)
 
+OUTPUT => "pizza"
+		  "hamburger"
+		  "hotdog"
+		  "muffin"
+		  "pudding"
 
-
-
+"""
