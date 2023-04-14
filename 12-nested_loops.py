106b120ac78ca4dee5a3e3328f633ea1059a58dd
JustJeeCode <michellestanley@michelles-air.home>
2023-04-14 20:50:05 +1000

    Added documentation to file

--- 12-nested_loops.py
+++ 12-nested_loops.py
@@ -1,19 +1,45 @@
-# Nested loops = The "inner loop" will finish all of it's iterations before 
-# 				 finishing one iteration of the "outer loop".
+"""
 
-# Gettings rows, columns and symbol
+A nested loop is the "inner loop" of 2 for loops, it will finish
+all iterations of itself before the outer loop will finish 1
+iteration.
+
+
+SYNTAX:
+
+for (condition):
+	code here
+	for (condition): <- will finish all iterations
+		code here 
+
+
+EXAMPLE:
+
+Here we will build a brick with rows and columns and a symbol
+of our choosing.
+
 rows = int(input("How many rows?: "))
 cols = int(input("How many columns?: "))
 symbol = input("Enter a symbol to use: ")
 
-# First for loop is the rows.
+
+The first for loop is for the rows.
 for i in range(rows):
-	# Nested for loop is the cols. Also it's good practice to use a j for 
-	# nested for loops.
+	
+	The nested for loop is for the columns and it is common to use
+	a j as an index for nested loops.
 	for j in range(cols):
-		# end="" makes the next iteration print to the same line.
-		print(symbol, end=" ")
-	# Printing a new line for every row.
-	print()
+		print(symbol, end=" ") <- End keeps printing to the same line.
+	print() <- We are printing a new line for every row.
+
+OUTPUT => (let's say we did 2x2 and used the # symbol.)
+=> # #
+   # #
+
+"""
+
+
+
+
 
 
