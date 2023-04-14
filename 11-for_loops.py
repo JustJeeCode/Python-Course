21cd79f4265ed60959124fa538f4d19acf17d47e
JustJeeCode <michellestanley@michelles-air.home>
2023-04-14 20:44:26 +1000

    Added documentation to file

--- 11-for_loops.py
+++ 11-for_loops.py
@@ -1,27 +1,52 @@
-# Time module allows us to access time.
+"""
+
 import time
 
-# For loop = a statement that will execute it's block of code
-# 		     a limited amount of times.
+For loop is a statement that will execute it's block of code
+a limited amount of times.
 
-#			 While loop = unlimited.
-# 			 For loop = limited.
+While loop = unlimited.
+For loop = limited.
 
-# i = index, can be whatever it's just a variable.
 
-# for i in range(10):
-# 	print(i+1)
+SYNTAX:
 
-# for i in range(50, 100+1, 2):
-# 	print(i)
+for (condition):
+	code here
 
-# We can iterate through data too. Here is a string.
-# for i in "JustJeeCode":
-# 	print(i)
 
-# 				  start,end,increment
-for seconds in range(10, 0, -1):
+EXAMPLE:
+
+ counter
+	v        go to
+for i in range(10): <- i just stands for index which is just a temporary variable
+	print(i+1)
+
+OUTPUT => 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+
+			 start  end  increment
+for i in range(50, 100+1, 2):
+	print(i)
+
+OUTPUT => 50, 52, 54, 56... 98, 100
+
+
+We can iterate through data too. Here is a string.
+Each character counts as an index.
+
+for i in "JustJeeCode":
+	print(i)
+
+OUTPUT => "J, u, s, t, j, e, e, c, o, d, e" 
+
+	variable
+   	   v	      
+for seconds in range(10, 0, -1): <- We are counting down.
 	print(seconds)
 	time.sleep(1)
+
 print("Happy New Year!")
+
+OUTPUT => 10, 9, 8... 2, 1, "Happy New Year!"
 
