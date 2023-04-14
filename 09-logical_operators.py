e7d15e97c7ace112e63238f25196ef45b619604b
JustJeeCode <michellestanley@michelles-air.home>
2023-04-14 20:34:19 +1000

    Added documentation to file

--- 09-logical_operators.py
+++ 09-logical_operators.py
@@ -1,16 +1,58 @@
-# Logical operators (and, or, not) = used to check if two or more conditional
-# statements
+"""
+
+Logical operators are operators used to check two 
+or more conditional statements.
+
+Logical operators include: and, or, not
+
+
+SYNTAX:
+	
+if (condition) or/and if (condition):
+	code here.
+
+if not(condition):
+	code here.
+
+if not(conditon) or/and if (condition):
+	code here.
+
+
+HERE IS AN EXAMPLE:
 
 temp = int(input("What is the temperature outside?: "))
+^^^^
+Assigning the variable temp the integer input.
 
-# And, or logical operators.
-# if temp >= 0 and temp <= 30:
-# 	print("The temperature is good today, go outside.")
-# elif temp < 0 or temp > 30:
-# 	print("The temperature is bad today, stay inside.")
+Here we use the and logical operator:
+We are basically saying if temp is greater or equal to 0 and
+temp is less than or equal to 30 do this.
+
+if temp >= 0 and temp <= 30:
+	print("The temperature is good today, go outside.")
+
+
+Here we are using the or logical operator:
+We are basically saying else if temp is less than 0 or temp
+is greater then 30 do this.
+
+elif temp < 0 or temp > 30:
+	print("The temperature is bad today, stay inside.")
+
+
+Here we are using the not logical operator:
+We are basically saying if temp is NOT greater or equal to 0
+and temp is NOT less then or equal to 30 do this.
 
 if not(temp >= 0 and temp <= 30):
 	print("The temperature is bad today, stay inside.")
-# Or logical operator.
+
+
+Here we are using the not logical operator:
+We are basically saying if temp is NOT less than 0 or temp
+is NOT greater than 30 do this.
+
 elif not(temp < 0 or temp > 30):
 	print("The temperature is good today, go outside.")
+
+"""
