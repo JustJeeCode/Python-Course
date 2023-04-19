828b70e1d7d307e9edba77042b981d820a97955d
JustJeeCode <michellestanley@michelles-air.home>
2023-04-19 21:29:59 +1000

    Added documentation to file

--- 17-sets.py
+++ 17-sets.py
@@ -1,20 +1,42 @@
-# Set = collection which is unordered, unindexed. No duplicate values.
+"""
 
-# Set syntax.
-utensils = {"fork", "spoon", "knife", "knife", "knife"}
+Set = collection which is unordered, unindexed. No duplicate values.
+
+Set syntax:
+utensils = {"fork", "spoon", "knife", "knife", "knife"} 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+The duplicate knife values will be removed.
+
 dishes = {"bowl", "plate", "cup", "knife"}
 
-# Set functions:
-# utensils.add("napkin") # Adds an element.
-# utensils.remove("fork") # Removes an element.
-# utensils.clear() # Clears the set.
-# utensils.update(dishes) # Adds all elements found in dishes to utensils.
-# print(utensils.difference(dishes)) # Checks what utensils has that dishes doesn't.
-# print(utensils.intersection(dishes)) # Checks what utensils and dishes has in common.
+Set functions:
 
-# Creates a new set with all the elems found in utensils and dishes.
+Adds an element:
+utensils.add("napkin")
+
+removes a targeted element:
+utensils.remove("fork")
+
+clears the set:
+utensils.clear()
+
+Adds all elements found in dishes to utensils:
+utensils.update(dishes)
+
+Checks what utensils has that dishes doesn't:
+print(utensils.difference(dishes))
+
+Checks what utensils and dishes have in common:
+print(utensils.intersection(dishes))
+
+Creates a new set with all the elements found in utensils and dishes:
 dinner_table = utensils.union(dishes) 
 
-# We can loop through all elements in a set just like a list.
+We can loop through all elements in a set just like a list:
+
 for i in dinner_table:
-	print(i) # Unordered result, will not print duplicate values.
+	print(i)
+^^^^^^^^^^^^^^^^^^^^^^
+Unordered result, will not print duplicate values.
+
+"""
