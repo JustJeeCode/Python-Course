2e495a95172f13f5c189e5b8baaedb941c33f6fc
JustJeeCode <michellestanley@michelles-air.home>
2023-04-19 21:22:25 +1000

    Added documentation to file

--- 16-tuples.py
+++ 16-tuples.py
@@ -1,17 +1,36 @@
-# Tuple = collection which is ordered and unchangeable
-#		  used to group together related data.
+"""
 
-# Tuple syntax:
+Tuple = collection which is ordered and unchangeable
+		used to group together related data.
+
+Tuple syntax:
 student = ("JustJee", 25, "Male")
 
-# Tuple functions:
-#print(student.count("JustJee")) # Count how many of a value is in a list
-#print(student.index("Male")) # Find the index of a value
+Tuple functions:
 
-# We can also go through all index's like lists.
+Counts how many of a selected element is in a tuple:
+print(student.count("JustJee")) | OUTPUT => 1
+
+
+Finds the index value of a selected element in a tuple:
+print(student.index("Male")) | OUTPUT => 2
+
+
+We can also go through all index's like lists:
+
 for i in student:
 	print(i)
 
-# We can check if a value is in our tuple.
+OUTPUT => "JustJee"
+		   25
+		  "Male"
+
+
+We can check if a value is in our tuple:
+
 if "JustJee" in student:
 	print("JustJee is here.")
+
+"""
+
+
