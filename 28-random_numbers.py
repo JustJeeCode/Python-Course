6ce8b7087d6314e1a432e807083b14ff9fef13e8
JustJeeCode <michellestanley@Michelles-MacBook-Air.local>
2023-04-20 13:15:20 +1000

    Added 28-random_numbers.py

---
+++ 28-random_numbers.py
@@ -0,0 +1,23 @@
+"""
+Random Module in Python.
+
+import random
+
+
+EXAMPLE - Random numbers:
+
+x = random.randint(1, 6) <- Random number between 1 - 6.
+y = random.random() <- Random floating number between 0 - 1.
+
+
+EXAMPLE - Random items:
+
+my_list = ['rock', 'paper', 'scissors']
+z = random.choice(my_list) <- Random element in the list.
+
+
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"]
+random.shuffle(cards) <- Shuffles the cards list.
+
+
+"""
