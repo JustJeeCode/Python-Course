23047d14d309a7a6dc25e80f9e2441c38ec75f19
JustJeeCode <michellestanley@Michelles-MacBook-Air.local>
2023-04-20 12:14:40 +1000

    Added documentation to file

--- 21-return_statement.py
+++ 21-return_statement.py
@@ -1,11 +1,24 @@
-# Return statement = Functions send Python values/objects back to the caller.
-#					 These values/objects are known as the function's return value.
+"""
 
+Return statement = Functions send Python values/objects back to the caller.
+				   These values/objects are known as the function's return value.
+
+
+SYNTAX:
+
+return x <- 'x' here would be our data or variable.
+
+
+EXAMPLE:
 
 def multiply(number1, number2):
-	# result = number1 * number2 <- We don't need this.
-	return number1 * number2 # Return syntax.
+	return number1 * number2
 
-x = multiply(6, 8) # We can assign a function to a variable.
+x = multiply(6, 8) <- x = the return of the multiply function.
 
-print(x)
+We can also assign functions to variables, as python just sees a function
+as a variable.
+
+print(x) | OUTPUT => 48
+
+"""
