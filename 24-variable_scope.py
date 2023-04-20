0da39893b3455964cc0d5dba54069c0191c6aee4
JustJeeCode <michellestanley@Michelles-MacBook-Air.local>
2023-04-20 12:26:40 +1000

    Added documentation to file

--- 24-variable_scope.py
+++ 24-variable_scope.py
@@ -1,15 +1,27 @@
-# scope = The region that a variable is recognized.
-#		  A variable is only available fomr inside the reigion it is created.
-#		  A global and locally scoped versions of a variable can be created.
+"""
+
+Scope = The region that a variable is recognized.
+		A variable is only available from inside the reigion it is created.
+		A global and locally scoped versions of a variable can be created.
 
 
-name = "Just" # This variable is a global variable (available inside & outside functions.)
+name = "Just" 
+^^^^^^^^^^^^^
+This variable is a global variable (available inside & outside functions.)
+Because it was assigned in the main program, not a function.
 
 def display_name():
-	# This variable has local scope to the display_name function because it was
-	# declared inside of it (available only inside this function.)
 	name = "Jee"
+	^^^^^^^^^^^^
+	This variable has local scope because it was
+	declared inside of the display_name function (available only inside this function.)
+
 	print(name)
 
-display_name() # Prints the local variable before scoping to the global.
-print(name) # Will print "Just".
+
+display_name() <- Prints the local variable "Jee"
+
+print(name) <- Will print "Just", because the variable inside of display_name
+is a local variable, the print function has no access to it.
+
+"""
