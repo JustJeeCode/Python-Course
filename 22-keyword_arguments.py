a9fad415aae7ab3eeefefae46d3ebbc45b5cedfa
JustJeeCode <michellestanley@Michelles-MacBook-Air.local>
2023-04-20 12:18:03 +1000

    Added documentation to file

--- 22-keyword_arguments.py
+++ 22-keyword_arguments.py
@@ -1,13 +1,27 @@
-# Keyword arguments = arguments preceded by an identifier, when we pass them to a function. 
-#					  The order of the arguments doesn't matter, unlike positional arguments
-#					  Python knows the names of the arguments that our function receives.
+"""
+
+Keyword arguments = Arguments preceded by an identifier, when we pass them to a function. 
+					The order of the arguments doesn't matter, unlike positional arguments
+					Python knows the names of the arguments that our function receives.
+
+
+EXAMPLE OF POSITIONAL ARGUMENTS:
 
 def hello(first, middle, last):
 	print("Hello " + first + " " + middle + " " + last)
 
-# hello("Code", "Jee", "Just") Here the order matters so our first argument = "Code"
-# and our last argument = "Just" which is the wrong way round.
+hello("Code", "Jee", "Just") | OUTPUT => "Hello Code Jee Just"
 
-hello(last="Code", middle="Jee", first="Just") # Here we ordered them wrong but since
-# we are using keyword arguments our first argument = "Just" and our last = "Code" which
-# is the right way round. :)
+Here the order matters so our first argument = "Code"
+and our last argument = "Just" which is the wrong way round.
+
+
+EXAMPLE OF KEYWORD ARGUMENTS:
+
+hello(last="Code", middle="Jee", first="Just") | OUTPUT => "Hello Just Jee Code"
+
+Here we ordered them wrong but since we are using keyword 
+arguments our argument first = "Just" and our argument last = "Code" which
+is the right way round. :)
+
+"""
