2907bd5d241de094dde86c36c177051e43a58ca2
JustJeeCode <michellestanley@Michelles-MacBook-Air.local>
2023-04-20 12:06:45 +1000

    Added documentation to file

--- 19-indexing.py
+++ 19-indexing.py
@@ -1,19 +1,46 @@
-# Index operator [] = gives access to a sequence's element (str, list, tuples, etc.)
+"""
+
+Index operator [] = gives access to a sequence's element 
+We can use the index operator in - str, list, tuples, etc.
+
+
+SYNTAX:
+
+Inside we have a number or
+we have a start, stop, step.
+ v
+[x] <- Number.
+
+[x:y:z] <- Start, stop, step.
+
+
+EXAMPLE: For a single number.
 
 name = "just Jee!"
 
-# if 'J' is lower.
-#if name[0].islower():
-	# Capitalizing our name.
-	# name = name.capitalize()
+if 'J' is lower:
+if name[0].islower(): <- name[0] = "j"
+	Capitalizing our name.
+	name = name.capitalize() <- We are using the capitalize function on our name variable.
 
-# If our index starts at 0 we can leave it blank.
-first_name = name[:4].upper()
-# If our index goes all the way to the end we can leave it blank.
-last_name = name[5:].lower()
-# If we want to get the last character we can use a negative index.
-last_character = name[-1] # -2 = 'e'
+OUTPUT => "Just Jee"
 
-print(first_name)
-print(last_name)
-print(last_character)
+
+EXAMPLE: For start, stop, step.
+
+first_name = name[:4].upper() <- If our index starts at 0 we can leave it blank.
+
+OUTPUT => first_name = "JUST"
+
+
+last_name = name[5:].lower() <- If our index goes all the way to the end we can leave it blank.
+
+OUTPUT => last_name = "jee"
+
+
+If we want to get the last character we can use a negative index:
+last_character = name[-1] <- An index of [-2] = 'e'
+
+OUTPUT => last_character = "!"
+
+"""
