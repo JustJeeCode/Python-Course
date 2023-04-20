8687d6912aaa1211fb56b8c11dfacb5de5e1edd6
JustJeeCode <michellestanley@Michelles-MacBook-Air.local>
2023-04-20 12:42:49 +1000

    Added documentation to file

--- 26-kwargs.py
+++ 26-kwargs.py
@@ -1,14 +1,20 @@
-# **kwargs = parameter that will pack all arguments into a dictionary
-#            useful so that a function can accept a varying amount of keyword arguments.
+"""
+**kwargs = Parameter that will pack all arguments into a dictionary
+           useful so that a function can accept a varying amount of keyword arguments.
 
-# ** packs all key, value pairs into the dictionary defined as "kwargs".
-# kwargs is just the name of our dictionary, we can change it.
+** packs all key, value pairs into our dictionary defined as "kwargs".
+kwargs is just the name of our dictionary, we can change it.
+
 def hello(**kwargs):
-	#print("Hello " + kwargs['first'] + " " + kwargs['last'])
+	print("Hello " + kwargs['first'] + " " + kwargs['last'])
 	print("Hello", end = " ")
 
 	for key, value in kwargs.items():
 		print(value, end = " ")
 
-# Packs all keyword arguments into key, value pairs into the kwargs dictionary.
+Packs all keyword arguments into key, value pairs into the kwargs dictionary:
+
 hello(title = "Mr", first = "Just", middle = "Jee", last = "Code") 
+
+OUTPUT => "Hello Just Code"
+		  "Hello Mr Just Jee Code"
