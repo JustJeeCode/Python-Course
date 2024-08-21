9357710fc9012e3399c879d0cc0ca99078ad0b2a
JustJeeCode <giorpieri@gmail.com>
2024-08-21 10:56:39 +1000

    Added variable.py

---
+++ variable.py
@@ -0,0 +1,49 @@
+# Variable = A container for a value (string, integer, float, boolean)
+# 			 a variable behaves as if it was the value it contains.
+
+# Strings (quotes)
+first_name = "Just"
+food = "hotdogs"
+email = "JustJee@fake.com"
+
+# print(f"Hello {first_name}")
+# print(f"You like {food}")
+# print(f"Your email is: {email}")
+
+# Integers (no quotes)
+age = 25
+quantity = 3
+num_of_students = 30
+
+# print(f"You're {age} years old")
+# print(f"You're buying {quantity} items")
+# print(f"Your class has {num_of_students} students")
+
+# Floats (contains decimal)
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# print(f"The price is ${price}")
+# print(f"Your gpa is: {gpa}")
+# print(f"You ran {distance}km")
+
+# Booleans (True/False)
+is_student = True
+for_sale = True
+is_online = True
+
+# print (f"Are you a student?: {is_online}")
+
+# # If statements tend to use Booleans
+# if is_online:
+# 	print ("That item is for sale!")
+# else:
+# 	print("That item is not for available.")
+
+# Examples
+username = "JustJee"
+age = 25
+exam_result = 7.5
+is_alive = True
+
