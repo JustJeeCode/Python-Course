bed5eeb9604d5f9557dd70a0b483bbd83f100e79
JustJeeCode <giorpieri@gmail.com>
2024-08-22 11:57:00 +1000

    Added type_casting.py

---
+++ type_casting.py
@@ -0,0 +1,15 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+name = "JustJee"
+age = 25
+gpa = 5.5
+is_student = True
+
+# Here we are typecasting our variables
+name = bool(name)
+age = float(age)
+gpa = int(gpa)
+is_student = str(is_student)
+
+print(is_student)
