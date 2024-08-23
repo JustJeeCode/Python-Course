49d950864d4c2a0019f94da1e6dd2ee4b88ac5ca
JustJeeCode <giorpieri@gmail.com>
2024-08-23 11:11:50 +1000

    Added arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,40 @@
+
+# friends = 10
+
+# friends = friends + 1
+# friends += 1
+# friends = friends - 2
+# friends -= 2
+# friends = friends * 3
+# friends *= 3
+# friends = friends / 2
+# friends /= 2
+# friends = friends ** 2 # power
+# friends **= 2
+# remainder = friends % 3 # remainder - can be used to find even/odd numbers
+
+# print(remainder)
+
+# x = 3.14
+# y = 4
+# z = 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4, 3)
+# result = max(x, y, z)
+# result = min(x, y, z)
+
+# print(result)
+
+import math
+
+x = 9.9
+
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(x)
+# result = math.ceil(x)
+result = math.floor(x)
+
+print(result)
