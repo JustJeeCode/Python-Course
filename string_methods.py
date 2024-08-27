3b7b783ac46948ff6addc79e550fddbecec7ccf5
JustJeeCode <giorpieri@gmail.com>
2024-08-27 11:53:59 +1000

    Added string_methods.py

---
+++ string_methods.py
@@ -0,0 +1,18 @@
+
+# name = input("Enter your full name: ")
+phone_number = input("Enter your phone number: ")
+
+# result = len(name)
+# result = name.find("o")
+# result = name.rfind("q")
+# name = name.capitalize()
+# name = name.upper()
+# name = name.lower()
+# result = name.isdigit() # str has to be only digits
+# result = name.isalpha() # str has only letters
+# result = phone_number.count("-")
+phone_number = phone_number.replace("-", "")
+
+# print(help(str))
+
+print(phone_number)
