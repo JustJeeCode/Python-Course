c732e432d59f4fa236918220644697d47e484d9f
JustJeeCode <giorpieri@gmail.com>
2024-08-29 12:08:43 +1000

    Added list_set_tuple.py

---
+++ list_set_tuple.py
@@ -0,0 +1,54 @@
+# Collection = single "variable" used to store multiple values
+#	List = [] ordered and changeable. Duplicates OK
+#	Set = {} unordered and immutable, but Add/Remove OK. No duplicates
+#	Tuple = () ordered and changeable. Duplicates OK. FASTER
+
+
+# fruits = ["apple", "orange", "banana", "coconut"]
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits)
+
+# fruits[0] = "pineapple"
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits.insert(0, "pineapple")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# print(fruits.index("apple"))
+# print(fruits.count("banana"))
+
+# for fruit in fruits:
+	# print(fruit)
+
+
+# fruits = {"apple", "orange", "banana", "coconut"}
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("coconut" in fruits)
+
+# fruits.add("pineapple")
+# fruits.remove("apple")
+# fruits.pop() # removes first elements
+# fruits.clear()
+
+
+fruits = ("apple", "orange", "banana", "coconut", "coconut")
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("banana" in fruits)
+
+# print(fruits.index("apple"))
+# fruits.count("coconut")
+
+# for fruit in fruits:
+# 	print(fruit)
+
+print(fruits)
+
+
+
