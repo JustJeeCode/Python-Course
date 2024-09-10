d3d9bff4b4979c3ee555526186a258f28298e627
JustJeeCode <giorpieri@gmail.com>
2024-09-10 17:42:28 +1000

    Working on wordle.py

--- wordle.py
+++ wordle.py
@@ -10,32 +10,59 @@ board = [["⬜", "⬜", "⬜", "⬜", "⬜"],
 		 ["⬜", "⬜", "⬜", "⬜", "⬜"],
 		 ["⬜", "⬜", "⬜", "⬜", "⬜"]]
 guesses = []
+running = True
 
-# Word generator
-randomWord = random.randint(0, 5850)
 
-while randomWord % 5 != 0:
-	randomWord = random.randint(0, 5850)
-
-with open("words.txt") as file:
-	words = file.read()
-
-word = words[randomWord:randomWord+5]
-print(word)
-
-# Game loop
-while True:
+def display_board():
 	for row in board:
 		for element in row:
 			print(element, end=" ")
 		print()
 
-	guess = input("Enter a word: ").lower()
-	guesses.append(guess)
 
+def random_word():
+	randNum = random.randint(0, 5850)
+
+	while randNum % 5 != 0:
+		randNum = random.randint(0, 5850)
+
+	with open("words.txt") as file:
+		words = file.read()
+
+	return words[randNum:randNum+5]
+	
+
+def guess_checker(guess):
 	if guess == word:
-		print(f"You guessed the word in: {len(guesses)} attempt/s!")
-		break
+		print(f"\nCorrect the word was {word}!")
+		return "won"
+
+	elif guess != word:
+		guesses.append(guess)
+
+		if len(guesses) >= 6:
+			print(f"\nIncorrect, you have run out of guesses...\nThe word was {word}.")
+			return "lost"
+
+
+
+while running:
+	display_board()
+
+	word = random_word()
+	print(word)
+
+	guess = input(f"\n>>> ").upper()
+	result = guess_checker(guess)
+
+	if result == "won" or result == "lost":
+		running = False
+
+	
+
+
+
+
 
 
 
