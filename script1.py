e1775d6d086edb3a8c38e238480666cc9c2322af
JustJeeCode <giorpieri@gmail.com>
2024-09-17 08:38:37 +1000

    Added if_name_main.py

---
+++ script1.py
@@ -0,0 +1,13 @@
+
+def favourite_food(food):
+	print(f"Your favourite food is {food}")
+
+
+def main():
+	print("This is script1")
+	favourite_food("Pizza")
+	print("Goodbye!")
+
+
+if __name__ == '__main__':
+	main()
---
+++ script2.py
@@ -0,0 +1,14 @@
+from script1 import *
+
+def favourite_drink(drink):
+	print(f"Your favourite drink is {drink}")
+
+
+def main():
+	print("This is script2")
+	favourite_food("Sushi")
+	favourite_drink("Coffee")
+	print("Goodbye")
+
+if __name__ == '__main__':
+	main()
